231287b6436905c3da78bfb5d34bb297db499231
Константин Клищук <kostyak13k@gmail.com>
2021-01-22 21:51:18 +0200

    - remove unused code

--- app/src/main/java/od/konstantin/myapplication/di/components/AppComponent.kt
+++ app/src/main/java/od/konstantin/myapplication/di/components/AppComponent.kt
@@ -5,9 +5,6 @@ import dagger.BindsInstance
 import dagger.Component
 import od.konstantin.myapplication.data.MoviesRepository
 import od.konstantin.myapplication.di.modules.MoviesApiModule
-import od.konstantin.myapplication.ui.moviedetails.MovieDetailsComponent
-import od.konstantin.myapplication.ui.movieslist.MoviesListComponent
-import od.konstantin.myapplication.ui.movieslist.page.MoviesListPageComponent
 import javax.inject.Singleton
 
 @Singleton
